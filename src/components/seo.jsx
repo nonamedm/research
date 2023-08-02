c4216b8cac53d8cf79ce944868c232597d5ee2df
nonamedm <nonamedm@naver.com>
2023-08-02 14:52:08 +0900

    타이틀수정

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -22,7 +22,7 @@ SEO.propTypes = {
 }
 
 SEO.defaultProps = {
-    title: "Flone",
+    title: "RF",
     titleTemplate: "Product Page",
     description: "Product page of flone react minimalist eCommerce template.",
 };
